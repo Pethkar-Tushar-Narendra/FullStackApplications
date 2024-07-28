c7c70de9ef71a509fd0ed45bac82fa2ebec57b36
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-07-28 17:50:33 +0530

    Done adding backend files

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
